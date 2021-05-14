f33d505cf4cb3558fc979ab1a91836720d00e873
teomrd <mironidis.gr@gmail.com>
2021-05-14 20:44:39 +0100

    Init the tree and some semantics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
+import Tree from "./components/Tree/Tree";
 import "./App.css";
 
 const App = () => {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <header style={{ marginBottom: "20px" }}>
+        <h1
+          style={{
+            marginBottom: "5px",
+          }}
+        >
+          React + D3
+        </h1>
+        <div>
+          <i>are friends </i> 😈
+        </div>
+      </header>
+      <main>
+        <Tree />
+      </main>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Tree/Tree.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tree = () => {
+  return <div>hello from tree</div>;
+};
+
+export default Tree;
