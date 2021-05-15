07c2042f93b336962697487d11dfbabc7523257f
teomrd <mironidis.gr@gmail.com>
2021-05-15 21:14:23 +0100

    Exit done. v1

--- src/components/NodeGraph/Tree.js
+++ src/components/NodeGraph/Tree.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Node from "./Node";
 import Link from "./Link";
 import "./style.css";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Tree = ({ nodes, links, width, height, onNodeClick, sourcePosition }) => {
   return (
@@ -19,7 +20,37 @@ const Tree = ({ nodes, links, width, height, onNodeClick, sourcePosition }) => {
             />
           );
         })}
-        {nodes.map((node) => {
+        <AnimatePresence>
+          {nodes.map((node) => {
+            const { id, x, y, name, _children, children } = node;
+            const { x: sx = x, y: sy = y } = sourcePosition;
+            const color = "lightblue";
+            const r = 30;
+            const isClickable = !!(_children || children);
+            return (
+              <motion.g
+                key={id}
+                className={`node ${isClickable ? "clickable" : ""}`}
+                onClick={() => onNodeClick(node)}
+                initial={{ x: sx, y: sy, scale: 0 }}
+                animate={{ x, y, scale: 1 }}
+                exit={{ x: sx, y: sy, scale: 0 }}
+                transition={{ duration: 0.75 }}
+              >
+                <circle r={r} style={{ fill: color }}></circle>
+                <text
+                  x="0"
+                  dy={-r - 5}
+                  textAnchor="middle"
+                  style={{ fillOpacity: 1 }}
+                >
+                  {name}
+                </text>
+              </motion.g>
+            );
+          })}
+        </AnimatePresence>
+        {/* {nodes.map((node) => {
           const { id, x, y, name, _children, children } = node;
           const isClickable = !!(_children || children);
 
@@ -35,7 +66,7 @@ const Tree = ({ nodes, links, width, height, onNodeClick, sourcePosition }) => {
               onNodeClick={() => onNodeClick(node)}
             />
           );
-        })}
+        })} */}
       </g>
     </svg>
   );
