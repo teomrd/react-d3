671c9c51f9674ac2dda13a095f8ca35119f4871e
teomrd <mironidis.gr@gmail.com>
2021-05-17 21:30:42 +0100

    NodeGraph size in props

--- src/components/NodeGraph/index.js
+++ src/components/NodeGraph/index.js
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 import d3 from "d3";
 import data from "../../data/flare.json";
 import Tree from "./Tree";
 import { idifyTree } from "../../utils/treeStructures";
-
-const margin = { top: 50, right: 50, bottom: 50, left: 50 };
-const width = 960 - margin.right - margin.left;
-const height = 800 - margin.top - margin.bottom;
-const fixedDepth = 200;
 
 const toggleChildren = (self) => {
   if (self.children) {
@@ -30,14 +26,13 @@ const fullyCollapseTreeOrNodes = (d) =>
     ? d.forEach(collapseChildrenToTheEnd)
     : collapseChildrenToTheEnd(d);
 
-const tree = d3.layout.tree().size([height, width]);
-
 const root = idifyTree(data);
 
-const NodeGraph = () => {
+const NodeGraph = ({ width, height, fixedDepth }) => {
   const [myNodes, setNodes] = useState([]);
   const [myLinks, setLinks] = useState([]);
   const [sourcePosition, setSourcePosition] = useState({});
+  const tree = d3.layout.tree().size([height, width]);
 
   const update = (source) => {
     setSourcePosition({
@@ -75,6 +70,18 @@ const NodeGraph = () => {
       onNodeClick={handleNodeClick}
     />
   );
+};
+
+NodeGraph.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  fixedDepth: PropTypes.number,
+};
+
+NodeGraph.defaultProps = {
+  width: 960,
+  height: 800,
+  fixedDepth: 200,
 };
 
 export default NodeGraph;
