afa5338c9175c8de609f6d0370ca57d0ac43ffd8
teomrd <mironidis.gr@gmail.com>
2021-06-12 21:45:27 +0100

    playground: add nodes to svg

--- src/components/Playground/Playground.js
+++ src/components/Playground/Playground.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAvailableWidth } from "../NodeGraph/useAvailableWidth";
+
+const playgroundContainerSvgId = "playground-svg";
 
 const Playground = () => {
+  const [nodes, setNodes] = useState([]);
+  const width = useAvailableWidth(`#${playgroundContainerSvgId}`);
+
+  const handleClick = (e) => {
+    const offsetTop = document.querySelector(`#${playgroundContainerSvgId}`)
+      .parentNode.offsetTop;
+
+    setNodes([
+      ...nodes,
+      {
+        x: e.clientX,
+        y: e.clientY - offsetTop,
+      },
+    ]);
+  };
+
   return (
     <div>
-      <h2>Playground</h2>
+      <svg id="playground-svg" width={width} height="800" onClick={handleClick}>
+        {nodes.map((node, i) => (
+          <circle key={i} cx={node.x} cy={node.y} r="10" fill="red" />
+        ))}
+      </svg>
     </div>
   );
 };
